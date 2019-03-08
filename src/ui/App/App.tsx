d7f586f8a8dd2c8e5181ba16c8cecb9bef217d27
klequis <klequis@gmail.com>
2019-03-07 17:03:25 -0800

    initial functionality working

---
+++ src/ui/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hello from 'ui/Hello'
+
+const App = () => {
+  return (
+    <Hello
+      name='Typescript'
+      enthusiasmLevel={10}
+    />
+  )
+}
+
+export default App
